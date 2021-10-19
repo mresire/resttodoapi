e1a161afc40b29a290f507a317348bd449cd497b
mresire <mresire@gmail.com>
2021-10-19 16:22:15 +0200

    collected static files locally

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -145,6 +145,14 @@ SECRET_KEY = 'Wahala'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
