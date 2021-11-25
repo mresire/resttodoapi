04df0f3aad191d9305536fa481d9b6dcc71aff14
mresire <mresire@gmail.com>
2021-11-25 15:39:04 +0200

    Update settings.py

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -143,9 +143,11 @@ SECRET_KEY = 'Wahala'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
